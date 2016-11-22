38a408a90825d373efe8544d867f162ceea0e40c
Unknown <13995656568@163.com>
2016-11-22 23:42:01 +0800

    项目骨架搭建

--- src/main/java/org/papaorange/app/AMovieSpriderApplication.java
+++ src/main/java/org/papaorange/app/AMovieSpriderApplication.java
@@ -1,4 +1,4 @@
-package org.papaorange;
+package org.papaorange.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/org/papaorange/service/PageDownloader.java
@@ -0,0 +1,38 @@
+package org.papaorange.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.Iterator;
+import java.io.IOException;
+
+/**
+ * Created by papaorange on 2016/11/22.
+ */
+public class PageDownloader {
+    public Document download(String url) throws IOException {
+        return Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get();
+    }
+
+    public static void main(String[] args) {
+        try {
+            for (int i = 1; i < 5626; i++) {
+                try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+                System.err.println("Downloading Page:" + i + "...");
+                Document doc = new PageDownloader().download("https://rarbg.to/torrents.php?category=14;17;42;44;45;46;47;48&page=" + i);
+                Iterator<Element> iter = doc.getElementsByClass("lista2").iterator();
+                while (iter.hasNext()) {
+                    String str = (String) iter.next().toString();
+                    System.out.println(str);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
