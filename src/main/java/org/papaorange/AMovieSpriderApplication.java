41e4e963d9ca096f3bd3d506d2ac6e6b6dfbb23d
Unknown <13995656568@163.com>
2016-11-22 18:23:03 +0800

    初始化目录结构

---
+++ src/main/java/org/papaorange/AMovieSpriderApplication.java
@@ -0,0 +1,12 @@
+package org.papaorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMovieSpriderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMovieSpriderApplication.class, args);
+	}
+}
