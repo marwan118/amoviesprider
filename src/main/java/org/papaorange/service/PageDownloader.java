2659cc94a2d9c8f8180d71cadb69c2d26e38e9c4
Papaorange <13995656568@163.com>
2016-11-23 11:51:15 +0800

    优化

--- src/main/java/org/papaorange/service/PageDownloader.java
+++ src/main/java/org/papaorange/service/PageDownloader.java
@@ -3,6 +3,7 @@ package org.papaorange.service;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.Iterator;
 
@@ -14,20 +15,20 @@ import java.io.IOException;
  */
 public class PageDownloader {
     public Document download(String url) throws IOException {
-        return Jsoup.connect(url).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get();
+        return Jsoup.connect(url).header("User-Agent", "User-Agent,Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36").get();
     }
 
     public static void main(String[] args) {
         try {
-            for (int i = 1; i < 5626; i++) {
+            for (int i = 1; i < 10; i++) {
                 try {
-					Thread.sleep(1000);
+					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
                 System.err.println("Downloading Page:" + i + "...");
-                Document doc = new PageDownloader().download("https://rarbg.to/torrents.php?category=14;17;42;44;45;46;47;48&page=" + i);
-                org.jsoup.select.Elements elements = doc.getElementsByClass("lista2");
+                Document doc = new PageDownloader().download("https://rarbg.to/torrents.php?category=42;44;45;46;47;48&page=" + i);
+                Elements elements = doc.getElementsByClass("lista2");
                 if(elements.size()==0)
                 {
                 	System.out.println(doc);
