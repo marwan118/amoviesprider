8e34917b78745624d9c2bc3abde17240bd1a147b
Papaorange <13995656568@163.com>
2016-11-24 08:32:06 +0800

    格式化代码

--- src/main/java/org/papaorange/amoviesprider/app/AMovieSpriderApplication.java
+++ src/main/java/org/papaorange/amoviesprider/app/AMovieSpriderApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AMovieSpriderApplication {
+public class AMovieSpriderApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(AMovieSpriderApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+	SpringApplication.run(AMovieSpriderApplication.class, args);
+    }
 }
