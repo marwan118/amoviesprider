1718ea502d55b92e432607c6d453998cb390ee7e
Unknown <13995656568@163.com>
2017-01-24 10:37:54 +0800

    去掉无用import

--- src/main/java/org/papaorange/amoviesprider/app/AMovieSpriderApplication.java
+++ src/main/java/org/papaorange/amoviesprider/app/AMovieSpriderApplication.java
@@ -1,6 +1,5 @@
 package org.papaorange.amoviesprider.app;
 
-import org.papaorange.amoviesprider.control.GetMovieController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
