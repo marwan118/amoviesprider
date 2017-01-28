8a8db5a19f4f677aecb001a6fec91943e053e46d
Unknown <13995656568@163.com>
2017-01-29 03:29:12 +0800

    remove imports

--- src/main/java/org/papaorange/amoviesprider/control/GetMovieTorrentController.java
+++ src/main/java/org/papaorange/amoviesprider/control/GetMovieTorrentController.java
@@ -8,7 +8,6 @@ import org.papaorange.amoviesprider.db.DBAgent;
 import org.papaorange.amoviesprider.db.DBMgr;
 import org.papaorange.amoviesprider.service.TorrentDownloadTask;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
